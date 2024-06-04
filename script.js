f17146178ad9b77df51f8498a7ebb7c36a061964
Charlie Turner <c.w.turner@hotmail.com>
2024-06-04 21:45:23 +0100

    Remove CSS causing dialog to stay open on page load

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 
 showButton.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
 });
 
 closeButton.addEventListener("click", () => {
-    dialog.close();
+  dialog.close();
 });
