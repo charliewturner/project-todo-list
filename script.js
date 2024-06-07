a4d743da4a6dddf94e42333ecd4bae46d04b5238
Charlie Turner <c.w.turner@hotmail.com>
2024-06-07 16:24:27 +0100

    Add buttons to dialog and reference in script file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+const closeButton = document.querySelector("button-close");
+const newTaskButton = document.querySelector("new-task");
+
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
